22600c1c1d45d5d4647f614a9df65a89f500470d
Ajesh Sen Thapa <aj3sshh@gmail.com>
2024-05-10 18:17:28 +0545

    ci: test
    Release-As: 1.0.0

--- src/commitlint/__version__.py
+++ src/commitlint/__version__.py
@@ -3,4 +3,4 @@ This module contains the version information of the current commitlint.
 TODO: automatically bump version through CI.
 """
 
-__version__ = "0.2.1"
+__version__ = "0.2.1"  # x-release-please-version

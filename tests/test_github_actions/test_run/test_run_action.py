dccbae9ee66065a0aaabcbc46e46b8c094ccf63d
Ajesh Sen Thapa <ast@logpoint.com>
2024-07-26 15:29:22 +0545

    test

---
+++ tests/test_github_actions/test_run/test_run_action.py
@@ -0,0 +1,42 @@
+import json
+import os
+
+import pytest
+from unittest.mock import patch, mock_open
+
+from github_actions.action.run import run_action
+
+
+def set_github_env_vars():
+    # GitHub Action event env
+    os.environ["GITHUB_EVENT_NAME"] = "push"
+    os.environ["GITHUB_SHA"] = "commitlint_sha"
+    os.environ["GITHUB_REF"] = "refs/heads/main"
+    os.environ["GITHUB_WORKFLOW"] = "commitlint_ci"
+    os.environ["GITHUB_ACTION"] = "action"
+    os.environ["GITHUB_ACTOR"] = "actor"
+    os.environ["GITHUB_REPOSITORY"] = "opensource-nepal/commitlint"
+    os.environ["GITHUB_JOB"] = "job"
+    os.environ["GITHUB_RUN_ATTEMPT"] = "9"
+    os.environ["GITHUB_RUN_NUMBER"] = "8"
+    os.environ["GITHUB_RUN_ID"] = "7"
+    os.environ["GITHUB_EVENT_PATH"] = "/tmp/github_event.json"
+    os.environ["GITHUB_STEP_SUMMARY"] = "/tmp/github_step_summary"
+    os.environ["GITHUB_OUTPUT"] = "/tmp/github_output"
+
+    # GitHub Action input env
+    os.environ["INPUT_VERBOSE"] = "false"
+    os.environ["INPUT_FAIL_ON_ERROR"] = "true"
+
+
+@pytest.fixture(autouse=True)
+def setup_env():
+    set_github_env_vars()
+
+
+@patch.dict(os.environ, {**os.environ, "GITHUB_EVENT_NAME": "push"})
+def test__run_action__push_event():
+    payload = {"commits": [{"message": "feat: valid message"}]}
+    with patch("builtins.open", mock_open(read_data=json.dumps(payload))):
+        print(os.environ)
+        run_action()
--- tests/test_github_actions/test_utils/test_get_boolean_input.py
+++ tests/test_github_actions/test_utils/test_get_boolean_input.py
@@ -8,37 +8,37 @@ import pytest
 from github_actions.action.utils import get_boolean_input
 
 
+@patch.dict(os.environ, {"INPUT_TEST": "True"})
 def test__get_boolean_input__return_True_for_True():
-    with patch.dict(os.environ, {"INPUT_TEST": "True"}):
-        assert get_boolean_input("test") == True
+    assert get_boolean_input("test") == True
 
 
+@patch.dict(os.environ, {"INPUT_TEST": "TRUE"})
 def test__get_boolean_input__return_True_for_TRUE():
-    with patch.dict(os.environ, {"INPUT_TEST": "TRUE"}):
-        assert get_boolean_input("test") == True
+    assert get_boolean_input("test") == True
 
 
+@patch.dict(os.environ, {"INPUT_TEST": "true"})
 def test__get_boolean_input__return_True_for_true():
-    with patch.dict(os.environ, {"INPUT_TEST": "true"}):
-        assert get_boolean_input("test") == True
+    assert get_boolean_input("test") == True
 
 
+@patch.dict(os.environ, {"INPUT_TEST": "False"})
 def test__get_boolean_input__return_False_for_False():
-    with patch.dict(os.environ, {"INPUT_TEST": "False"}):
-        assert get_boolean_input("test") == False
+    assert get_boolean_input("test") == False
 
 
+@patch.dict(os.environ, {"INPUT_TEST": "FALSE"})
 def test__get_boolean_input__return_False_for_FALSE():
-    with patch.dict(os.environ, {"INPUT_TEST": "FALSE"}):
-        assert get_boolean_input("test") == False
+    assert get_boolean_input("test") == False
 
 
+@patch.dict(os.environ, {"INPUT_TEST": "false"})
 def test__get_boolean_input__return_False_for_false():
-    with patch.dict(os.environ, {"INPUT_TEST": "false"}):
-        assert get_boolean_input("test") == False
+    assert get_boolean_input("test") == False
 
 
+@patch.dict(os.environ, {"INPUT_TEST": "random"})
 def test__get_boolean_input__raises_type_error_for_unknown():
-    with patch.dict(os.environ, {"INPUT_TEST": "random"}):
-        with pytest.raises(TypeError):
-            get_boolean_input("test")
+    with pytest.raises(TypeError):
+        get_boolean_input("test")

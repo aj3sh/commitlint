520056015faa2eb63a88ac6e648761154e707c21
Ajesh Sen Thapa <aj3sshh@gmail.com>
2024-07-30 22:03:41 +0545

    test

--- tests/test_github_actions/test_run/test_run_action.py
+++ tests/test_github_actions/test_run/test_run_action.py
@@ -1,8 +1,11 @@
+# type: ignore
+# pylint: disable=all
 import json
 import os
+import subprocess
+from unittest.mock import call, mock_open, patch
 
 import pytest
-from unittest.mock import patch, mock_open
 
 from github_actions.action.run import run_action
 
@@ -34,9 +37,72 @@ def setup_env():
     set_github_env_vars()
 
 
+# integration test of run_action to ensure Github Action full functionality.
+
+
+@patch("subprocess.check_output", return_value="success")
 @patch.dict(os.environ, {**os.environ, "GITHUB_EVENT_NAME": "push"})
-def test__run_action__push_event():
-    payload = {"commits": [{"message": "feat: valid message"}]}
+def test__run_action__push_event_full_integration_test_for_all_valid_commits(
+    mock_check_output,
+):
+    payload = {
+        "commits": [
+            {"message": "feat: valid message"},
+            {"message": "fix(login): fix login message"},
+        ]
+    }
     with patch("builtins.open", mock_open(read_data=json.dumps(payload))):
-        print(os.environ)
         run_action()
+
+    assert mock_check_output.call_count == 2
+    expected_calls = [
+        call(
+            ["commitlint", "feat: valid message", "--hide-input"],
+            text=True,
+            stderr=subprocess.PIPE,
+        ),
+        call(
+            ["commitlint", "fix(login): fix login message", "--hide-input"],
+            text=True,
+            stderr=subprocess.PIPE,
+        ),
+    ]
+    mock_check_output.assert_has_calls(expected_calls, any_order=False)
+
+
+# @patch.dict(os.environ, {**os.environ, "GITHUB_EVENT_NAME": "push"})
+# def test__run_action__push_event_full_integration_test_for_partially_invalid_commits():
+#     payload = {
+#         "commits": [
+#             {"message": "feat: valid message"},
+#             {"message": "fix login message"},
+#         ]
+#     }
+#     with patch("builtins.open", mock_open(read_data=json.dumps(payload))):
+#         with pytest.raises(SystemExit):
+#             run_action()
+
+
+# @patch.dict(os.environ, {**os.environ, "GITHUB_EVENT_NAME": "push"})
+# def test__run_action__push_event_full_integration_test_for_all_invalid_commits():
+#     payload = {
+#         "commits": [
+#             {"message": "invalid message"},
+#             {"message": "fix login message"},
+#         ]
+#     }
+#     with patch("builtins.open", mock_open(read_data=json.dumps(payload))):
+#         with pytest.raises(SystemExit):
+#             run_action()
+
+
+# @patch.dict(os.environ, {**os.environ, "GITHUB_EVENT_NAME": "pull_request"})
+# def test__run_action__pr_event_full_integration_test_for_all_valid_commits():
+#     payload = {
+#         "commits": [
+#             {"message": "feat: valid message"},
+#             {"message": "fix(login): fix login message"},
+#         ]
+#     }
+#     with patch("builtins.open", mock_open(read_data=json.dumps(payload))):
+#         run_action()
